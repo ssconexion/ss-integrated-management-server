1f7b2e130b817f3179f92ec9550030804cd48d81
Angarn <angarn2876@gmail.com>
2026-02-04 20:38:01 +0100

    testing básico
    migrare todo lo relacionado a tests a la clase homónima

--- ss.Integrated.Management.Server/Program.cs
+++ ss.Integrated.Management.Server/Program.cs
@@ -5,13 +5,29 @@ namespace ss.Internal.Management.Server
 {
 	public static class Program
 	{
-		public const string ConnectionString = "";
 		public const string TournamentName = "SS26";  
 		
-		public static void Main(string[] args)
+		public static async Task Main(string[] args)
 		{
 			Console.WriteLine("hola server de jd");
 			
+			var matchType = Models.MatchType.EliminationStage;
+			
+			var autoRef = new AutoRef.AutoRef("32", "Furina", matchType);
+			
+			await Task.Run(async () => 
+			{
+				try 
+				{
+					await autoRef.StartAsync();
+				}
+				catch (Exception ex) 
+				{
+					Console.WriteLine($"Error en AutoRef: {ex.Message}");
+				}
+			});
+			
+			Console.WriteLine("adios server de jd");
 		}
 	}
 };
