e0a93b29ce119d328db3b324f163fbbf3de95fcd
Angarn <angarn2876@gmail.com>
2026-02-08 18:42:35 +0100

    startup

--- ss.Integrated.Management.Server/Program.cs
+++ ss.Integrated.Management.Server/Program.cs
@@ -1,5 +1,7 @@
-﻿using DotNetEnv;
+﻿using System.Globalization;
+using DotNetEnv;
 using ss.Internal.Management.Server.Discord;
+using ss.Internal.Management.Server.Localization;
 
 namespace ss.Internal.Management.Server
 {
@@ -12,6 +14,8 @@ namespace ss.Internal.Management.Server
 			Console.WriteLine("hola server de jd");
 
 			Env.Load();
+
+			I18n.CurrentCulture = new CultureInfo(Environment.GetEnvironmentVariable("LANGUAGE") ?? "en");
 			
 			var manager = new DiscordManager(Environment.GetEnvironmentVariable("DISCORD_BOT_TOKEN") ?? throw new InvalidOperationException());
 			await manager.StartAsync();
